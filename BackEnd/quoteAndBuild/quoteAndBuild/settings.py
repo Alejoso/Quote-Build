3215b070f300a70c983c4fa1cfa5165bf3b3146f
SimónSloan <ssgarciav@eafit.com>
2025-08-14 22:17:56 -0500

    Minnor changes

--- BackEnd/quoteAndBuild/quoteAndBuild/settings.py
+++ BackEnd/quoteAndBuild/quoteAndBuild/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
             'ENGINE': 'django.db.backends.mysql',
             'NAME': 'quoteandbuild',
             'USER': 'root',
-            'PASSWORD': 'Bebelindo2020-',
+            'PASSWORD': 'Ss30ma-.',
             'HOST': '127.0.0.1',
             'PORT': '3306',
         }
