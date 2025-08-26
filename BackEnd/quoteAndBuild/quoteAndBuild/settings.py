986f81576ab561ae1952a1dff743703d7617033a
SimónSloan <ssgarciav@eafit.com>
2025-08-26 09:33:42 -0500

    Database changes for being sqlite

--- BackEnd/quoteAndBuild/quoteAndBuild/settings.py
+++ BackEnd/quoteAndBuild/quoteAndBuild/settings.py
@@ -89,15 +89,11 @@ WSGI_APPLICATION = 'quoteAndBuild.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'quoteandbuild',
-            'USER': 'root',
-            'PASSWORD': 'Ss30ma-.',
-            'HOST': '127.0.0.1',
-            'PORT': '3306',
-        }
-    }
+    "default": {  # destino: SQLite
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    },
+}
 
 
 # Password validation
